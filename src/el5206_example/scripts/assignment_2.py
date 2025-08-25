c32947621440dfbfee4ba6b6955bbb6328148cd1
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-08-25 12:12:01 -0400

    added data and new teleop plot

--- src/el5206_example/scripts/assignment_2.py
+++ src/el5206_example/scripts/assignment_2.py
@@ -200,21 +200,3 @@ if __name__ == "__main__":
         node.move_to_goal()
     except rospy.ROSInterruptException:
         pass
-
-# rostopic pub /move_base_simple/goal geometry_msgs/PoseStamped "header:
-#   seq: 0
-#   stamp:
-#     secs: 0
-#     nsecs: 0
-#   frame_id: 'world'
-# pose:
-#   position:
-#     x: 1.0
-#     y: 2.0
-#     z: 0.0
-#   orientation:
-#     x: 0.0
-#     y: 0.0
-#     z: 0.0
-#     w: 1.0" 
-
